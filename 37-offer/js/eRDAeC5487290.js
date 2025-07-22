c003cb4e52e2395ee7d9bd646465eb7691079c25
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-07-22 14:35:19 -0300

    Add files via upload

---
+++ 37-offer/js/eRDAeC5487290.js
@@ -0,0 +1,48 @@
+(function() {
+          try {
+              const clickeventList = [{"compKey":"0ead096","misc":{"type":"button"}},{"compKey":"c581eb6","misc":{"type":"text"}},{"compKey":"295d888","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();(function() {
+          try {
+              const animationList = [{"key":"97bb8a2","type":"image"},{"key":"0ead096","type":"button"}];
+    
+              animationList.forEach((animationItem, index) => {
+                const { key, type } = animationItem;
+                const elementClass = type === "container" ? ".atomicat-container-" + key : ".atomicat-element-container-" + key;
+                const targetElement = document.querySelector(elementClass);
+
+
+                    const observer = new IntersectionObserver(entries => {
+                    entries.forEach(entry => {
+                            if (entry.isIntersecting) {
+                                targetElement.style.opacity = 1;
+                                targetElement.classList.add('atomicat-entrance-animation-' + key);
+                            } else if(animationItem?.misc?.hideOffscreen) {
+                                targetElement.classList.remove('atomicat-entrance-animation-' + key);
+                                targetElement.style.opacity = 0;
+                            }
+                        });
+                    });
+
+                    observer.observe(targetElement);
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
